c9e8696baf8c8026322875ad14df0ed9a2694161
KamiKaZe <128917461+Romykat81@users.noreply.github.com>
2023-05-28 13:31:15 +0200

    main file

---
+++ main.c
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+* struct print - print functions 
+* @type_arg: identifier
+* @f: pointer to printer functions
+* struct that stores pointers to print
+*/
+typedef struct print
+{
+    char *type_arg;
+    int (*f)(va_list, char *, unsigned int);
+}print_t
+
+int _printf(const char *format, ...);
+int print_chr(va_list arg, char *buf, unsigned int buf);
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
